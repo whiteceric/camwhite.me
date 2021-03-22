eb74e21947bd184fefb6da2f52a78670f25cea31
camwhite <whiteceric@gmail.com>
2021-03-21 23:01:07 -0400

    Fixed template loading to make editing project pages easier

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,21 +3,33 @@ from django.utils.text import slugify
 from shutil import copy
 from django.conf import settings
 from django.core.files import File
+from django.core.files.storage import FileSystemStorage
 import os
 
 # Create your models here.
 
+# class to ensure that we know the names of project html files
+class OverwriteStorage(FileSystemStorage):
+
+    def get_available_name(self, name, max_length=float('inf')):
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        return name if len(name) <= max_length else super().get_alternative_name()
+
+custom_storage = OverwriteStorage(location=settings.MEDIA_ROOT)
+
 class Project(models.Model):
     title = models.CharField(max_length=255, unique=True)
-    slugified_name = models.SlugField(max_length=510, blank=True, unique=True)
+    slugified_name = models.SlugField(max_length=510, blank=True, unique=True, editable=False)
     start_date = models.DateField()
     end_date = models.DateField()
     summary = models.TextField()
     link = models.CharField(max_length=255)
-    detail_source = models.FileField(upload_to='project_detail_source', null=True, blank=True, editable=False) 
+    detail_source = models.FileField(upload_to='project_detail_source', storage=custom_storage, null=True, blank=True, editable=False) 
 
     def save(self, *args, **kwargs):
         self.slugified_name = slugify(self.title)
+        super().save(*args, **kwargs)
         if not self.detail_source:
             # Copy the __template__ html file to a new html file with slugified_name as the name (+'.html')
             filename = f'{self.slugified_name}.html'
@@ -27,7 +39,6 @@ class Project(models.Model):
             the_file.close()
             detail_file = File(open(filepath, 'r'))
             self.detail_source.save(filename, detail_file)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
