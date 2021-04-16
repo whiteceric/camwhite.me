d25d081d1e72eb096c9ab2ecb7fdc8c8e9ec09a5
camwhite <whiteceric@gmail.com>
2021-04-16 06:50:22 -0400

    Fixed ProjectListItem center align issue

--- frontend/src/components/ProjectListItem/ProjectListItem.js
+++ frontend/src/components/ProjectListItem/ProjectListItem.js
@@ -4,15 +4,17 @@ import { Link } from "react-router-dom";
 import "./ProjectListItem.css";
 
 const ProjectListItem = ({ title, title_href, dates, summary, link }) => (
-  <Link className="project-item-link" to={title_href}>
-    <div className="ProjectListItem">
-      <div className="project-item-top-row">
-        <h1 className="project-title">{title}</h1>
-        <small className="dates-text">{dates}</small>
+  <div className="project-item-container">
+    <Link className="project-item-link" to={title_href}>
+      <div className="ProjectListItem">
+        <div className="project-item-top-row">
+          <h1 className="project-title">{title}</h1>
+          <small className="dates-text">{dates}</small>
+        </div>
+        <p className="summary-text">{summary}</p>
       </div>
-      <p className="summary-text">{summary}</p>
-    </div>
-  </Link>
+    </Link>
+  </div>
 );
 
 ProjectListItem.propTypes = {};
