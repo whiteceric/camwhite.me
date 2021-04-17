f1bb5bc67e9c95c2f0b4e18c0679a4268fbfc03f
camwhite <whiteceric@gmail.com>
2021-04-17 04:24:29 -0400

    Added link to Resume PDF from NavBar

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -5,30 +5,41 @@ import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
 
 import logo from "../../images/logo.png";
+import resume_pdf from "../../static/resume.pdf";
 
-const NavBar = () => (
-  <div className="NavBar">
-    <Navbar className="navbar-custom-bg" variant="dark" stick="top" expand="lg">
-      <Navbar.Brand>
-        <Link to="/">
-          <img alt="" src={logo} className="logo d-inline-block align-top" />
-        </Link>
-      </Navbar.Brand>
-      <Nav className="ml-auto">
-        <Nav.Link>
-          <Link className="navbar-link-item" to="/contact">
-            Resume
+const NavBar = () => {
+  const openResume = () => {
+    window.open(resume_pdf);
+  };
+  return (
+    <div className="NavBar">
+      <Navbar
+        className="navbar-custom-bg"
+        variant="dark"
+        stick="top"
+        expand="lg"
+      >
+        <Navbar.Brand>
+          <Link to="/">
+            <img alt="" src={logo} className="logo d-inline-block align-top" />
           </Link>
-        </Nav.Link>
-        <Nav.Link>
-          <Link className="navbar-link-item" to="/contact">
-            Contact
-          </Link>
-        </Nav.Link>
-      </Nav>
-    </Navbar>
-  </div>
-);
+        </Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link>
+            <button className="navbar-link-item" onClick={openResume}>
+              Resume
+            </button>
+          </Nav.Link>
+          <Nav.Link>
+            <Link className="navbar-link-item" to="/contact">
+              Contact
+            </Link>
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+};
 
 NavBar.propTypes = {};
 
