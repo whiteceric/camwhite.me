cc1b540882d143f11a9e4d31c6535fe9935f19f4
camwhite <whiteceric@gmail.com>
2021-04-17 15:35:28 -0400

    Removed react-bootstrap dependency (Made custom NavBar)

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./NavBar.css";
-import { Nav, Navbar } from "react-bootstrap"; // use bootstrap Navbar;
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
 import resume_pdf from "../../static/resume.pdf";
@@ -11,30 +10,17 @@ const NavBar = () => {
   };
   return (
     <div className="NavBar">
-      <Navbar
-        className="navbar-custom-bg"
-        variant="dark"
-        stick="top"
-        expand="lg"
-      >
-        <Navbar.Brand>
-          <Link to="/">
-            <img alt="" src={logo} className="logo d-inline-block align-top" />
-          </Link>
-        </Navbar.Brand>
-        <Nav className="ml-auto">
-          <Nav.Link>
-            <button className="navbar-link-item" onClick={openResume}>
-              Resume
-            </button>
-          </Nav.Link>
-          <Nav.Link>
-            <Link className="navbar-link-item" to="/contact">
-              Contact
-            </Link>
-          </Nav.Link>
-        </Nav>
-      </Navbar>
+      <Link to="/">
+        <img alt="" src={logo} className="logo" />
+      </Link>
+      <div className="navbar-links">
+        <button className="navbar-link-item" onClick={openResume}>
+          Resume
+        </button>
+        <Link className="navbar-link-item" to="/contact">
+          Contact
+        </Link>
+      </div>
     </div>
   );
 };
