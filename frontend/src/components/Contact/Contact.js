5fc35650554b28aeaaaa6dd5181c073162cbf8de
camwhite <whiteceric@gmail.com>
2021-04-17 04:09:51 -0400

    Filled in Contact Page

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Contact.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Contact.css";
 
 const Contact = () => (
   <div className="Contact">
-    Contact Component
+    <h1 className="section-title">Contact Me</h1>
+    <hr />
+    <h2 className="contact-link-title">
+      Email - <a href="mailto:whiteceric@gmail.com">whiteceric@gmail.com</a>
+    </h2>
+    <h2 className="contact-link-title">
+      Twitter -{" "}
+      <a target="_blank" href="https://twitter.com/camwhite271">
+        @camwhite271
+      </a>
+    </h2>
+    <h2 className="contact-link-title">
+      LinkedIn -{" "}
+      <a target="_blank" href="https://www.linkedin.com/in/camwhite1301/">
+        camwhite1301
+      </a>
+    </h2>
+    <h2 className="contact-link-title">
+      GitHub -{" "}
+      <a target="_blank" href="https://github.com/whiteceric">
+        whiteceric
+      </a>
+    </h2>
   </div>
 );
 
