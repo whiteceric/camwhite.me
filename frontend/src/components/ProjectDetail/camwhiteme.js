6e011174f9394774c9e8a1a67ae06af72a35269d
Cam White <whiteceric@gmail.com>
2021-04-18 13:31:46 +0000

    Added back the databases but split into Production & Dev
    Updated camwhite.me project

--- frontend/src/components/ProjectDetail/camwhiteme.js
+++ frontend/src/components/ProjectDetail/camwhiteme.js
@@ -8,26 +8,31 @@ const ProjectDetailView = () => {
         <p>
           After a few tutorial web projects, I decided to make this site my
           first deployed website. Decided to keep it simple for the first few
-          versions, but there are more features to come. Web development is not
+          versions, but eventually updated to using React for the frontend and using Django only for a backend REST API. Web development is not
           something I saw myself doing before this, but having really enjoyed
           this project I definitely plan to do more of it.
         </p>
         <h1>Technology</h1>
+	  <p>
+	  Currently, this project uses React for the frontend, the django-rest-framework module for the backend API, and a remote Ubuntu server (v20.04) for deployment on an Apache server.
+	  </p>
         <p>
           I went with the Django framework for two main reasons: First I know
           and love Python, making learning web development that much easier
           because I was using a familiar language. Second, the documentation on
           Django is extensive, so virtually any problem I came accross was
-          solvable by reading through APIs and forums. At first I wanted to use
-          the project to learn Javascript, but decided that it was not necessary
-          for such a simple site. So, the front end is almost plain HTML, with
-          Django working in the background providing some context variables. For
-          CSS, I wrote I few custom classes and referenced Bootstrap.
+          solvable by reading through APIs and forums.
         </p>
+	  <p>
+	  It is possible that React was a bit overkill for such a simple site, but I wanted to get experience with it before starting bigger projects, so I figured this was the best place to start.
+	  </p>
         <h1>Things I Learned</h1>
         <ul>
           <li>
             <p>Web Dev is Fun!</p>
+          </li>
+          <li>
+            <p>Basics of React and deploying static files for React using the whitenoise python module</p>
           </li>
           <li>
             <p>How to use Ubuntu from terminal to setup web server</p>
--- portfolio/models.py
+++ portfolio/models.py
@@ -25,21 +25,10 @@ class Project(models.Model):
     end_date = models.DateField()
     summary = models.TextField()
     link = models.CharField(max_length=255, null=True, blank=True)
-    detail_source = models.FileField(upload_to='', storage=custom_storage, null=True, blank=True, editable=False) 
 
     def save(self, *args, **kwargs):
-        print(settings.PROJECT_DETAIL_ROOT)
         self.slugified_name = slugify(self.title)
         super().save(*args, **kwargs)
-        if not self.detail_source:
-            # Copy the __template__ html file to a new html file with slugified_name as the name (+'.html')
-            filename = f'{self.slugified_name}.js'
-            filepath = os.path.join(settings.PROJECT_DETAIL_ROOT, filename)
-            the_file = open(filepath, 'w')
-            copy(os.path.join(settings.PROJECT_DETAIL_ROOT, 'templateDetailView.js'), filepath)
-            the_file.close()
-            detail_file = File(open(filepath, 'r'))
-            self.detail_source.save(filename, detail_file)
 
     def __str__(self):
         return self.title
