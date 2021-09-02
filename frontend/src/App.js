b9f939acd28114cc1bd78e2bb8f5ea2378906df3
camwhite <whiteceric@gmail.com>
2021-09-02 00:38:13 -0500

    Create Resume Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer/Footer";
 import AboutSection from "./components/AboutSection/AboutSection";
 import SkillList from "./components/SkillList/SkillList";
 import Contact from "./components/Contact/Contact";
+import ResumePage from "./components/ResumePage/ResumePage";
 import ProjectListItem from "./components/ProjectListItem/ProjectListItem";
 import ProjectDetail from "./components/ProjectDetail/ProjectDetail";
 import { useFetch } from "./components/useFetch";
@@ -21,44 +22,8 @@ function App() {
   return (
     <Router>
       <NavBar />
-      <Route
-        path="/"
-        exact
-        render={(props) => (
-          <>
-            <Header />
-            <AboutSection text={bio.ABOUT} />
-            <SkillList
-              title="Skills"
-              list_side="left"
-              skills={bio.SKILLS != null ? bio.SKILLS : []}
-            />
-            <SkillList
-              title="Tools"
-              list_side="right"
-              skills={bio.TOOLS != null ? bio.TOOLS : []}
-            />
-            <h1 className="section-title">Projects</h1>
-            {projects.map(function (project, index) {
-              return (
-                <div key={index}>
-                  <ProjectListItem
-                    title={project.title}
-                    title_href={`/project/${project.slugified_name}`}
-                    dates={
-                      formatDate(project.start_date) +
-                      " to " +
-                      formatDate(project.end_date)
-                    }
-                    summary={project.summary}
-                    link={project.link}
-                  />
-                </div>
-              );
-            })}
-          </>
-        )}
-      />
+      <Route path="/" exact render={(props) => <h1>Home Page</h1>} />
+      <Route path="/resume" exact render={(props) => <ResumePage />} />
       <Route path="/contact" exact render={(props) => <Contact />} />
       <Route path="/project/:slugified_title" children={<ProjectDetail />} />
       <Footer />
--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo_3.png";
 import resume_pdf from "../../static/resume.pdf";
 
-const NavBar = () => {
+const NavBar = (link_pdf_resume) => {
   const openResume = () => {
     window.open(resume_pdf);
   };
@@ -14,9 +15,15 @@ const NavBar = () => {
         <img alt="" src={logo} className="logo" />
       </Link>
       <div className="navbar-links">
-        <button className="navbar-link-item" onClick={openResume}>
-          Resume
-        </button>
+        {useLocation().pathname == "/resume" ? (
+          <button className="navbar-link-item" onClick={openResume}>
+            Resume (PDF)
+          </button>
+        ) : (
+          <Link className="navbar-link-item" to="/resume">
+            Resume
+          </Link>
+        )}
         <Link className="navbar-link-item" to="/contact">
           Contact
         </Link>
@@ -27,6 +34,8 @@ const NavBar = () => {
 
 NavBar.propTypes = {};
 
-NavBar.defaultProps = {};
+NavBar.defaultProps = {
+  link_pdf_resume: false,
+};
 
 export default NavBar;
